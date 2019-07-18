ecd7e3c785326af7fb14820d82d9c0c6e8b796d4
shubhamkokul <shubhamkokul@gmail.com>
2019-07-18 16:47:19 -0400

    Push

--- app/src/main/java/com/mumbai/financial/financialplanner/Expenses.java
+++ app/src/main/java/com/mumbai/financial/financialplanner/Expenses.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -51,7 +52,19 @@ public class Expenses extends Fragment {
             }
         });
 
+        expenseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openDetailView(position);
+            }
+        });
+
 
         return view;
+    }
+    public void openDetailView(int position){
+        Intent intent = new Intent(getActivity(), ExpenseDetailView.class);
+        intent.putExtra("position", position);
+        startActivity(intent);
     }
 }
